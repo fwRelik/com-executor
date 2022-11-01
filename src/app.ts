d0f191d1e28eee189079a6cbf437566eba0266cc
fwRelik <denis.isaev.fw@gmail.com>
2022-11-01 16:09:04 +0300

    Prompt service

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { Promptservice } from './core/prompt/prompt.service';
+
+export class App {
+	async run() {
+		const res = await new Promptservice().input<number>('Число', 'number');
+		console.log(res);
+	}
+}
+
+const app = new App();
+app.run();
